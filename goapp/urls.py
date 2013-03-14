ce4f0bad91e423250dee86b3122e60da297015d6
Simon de Haan <simon@praekeltfoundation.org>
2013-03-14 21:19:11 +0200

    minimal app

--- goapp/urls.py
+++ goapp/urls.py
@@ -4,6 +4,9 @@ from django.conf.urls import patterns, include, url
 # from django.contrib import admin
 # admin.autodiscover()
 
+from goapp import views
+
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'goapp.views.home', name='home'),
@@ -14,4 +17,5 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+    url(r'^messages\.json$', views.messages),
 )
---
+++ goapp/views.py
@@ -0,0 +1,35 @@
+import os
+import requests
+import json
+
+from django.http import HttpResponse
+
+
+def reply(message_id, content, session_event="resume"):
+
+    go_access_token = os.environ['GO_ACCESS_TOKEN']
+    go_account_key = os.environ['GO_ACCOUNT_KEY']
+    go_conversation_key = os.environ['GO_CONVERSATION_KEY']
+    message_url = 'http://go.vumi.org/api/v1/go/http_api/%s/messages.json' % (
+        go_conversation_key,)
+
+    payload = {
+        "in_reply_to": message_id,
+        "content": content,
+        "session_event": session_event,
+    }
+    requests.put(message_url, auth=(go_account_key, go_access_token),
+        data=json.dumps(payload))
+
+
+def messages(request):
+    if request.method == 'POST':
+        received_message = json.loads(request.body)
+        content = received_message['content']
+        message_id = received_message['message_id']
+        if not content:
+            reply(message_id, 'Hi, what is your name?')
+        else:
+            reply(message_id, 'Thanks %s!' % (content,), session_event="close")
+        return HttpResponse("")
+    return HttpResponse("Sorry, I only do POSTs")
